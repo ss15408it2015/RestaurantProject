348d919844a1b7d09cf7e2bf56f97bc30630476b
Shubham Sharma <Shubham@cloudthing.com>
2020-01-21 10:40:59 +0530

    ci

--- XUnitTestForRestaurantWebAPI/ControllerTesting/RestaurantControllerTests.cs
+++ XUnitTestForRestaurantWebAPI/ControllerTesting/RestaurantControllerTests.cs
@@ -75,7 +75,7 @@ namespace XUnitTestForRestaurantWebAPI.ControllerTesting
         {
             //Arrange
             _mockRepo.Setup(repo => repo.GetRestaurantByID(4))
-                .Returns(Task.FromResult(new Restaurant() { ID = 4 }));
+                .Returns(Task.FromResult(new Restaurant() { ID = 4,city = "Kota" }));
 
             //Act
             var result = await _controller.GetRestaurantByID(500);
